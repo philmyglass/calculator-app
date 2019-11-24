6a65d21518ac3e821e4c69e5fe7bfa19f7987946
Phil Green <philofgreen81@gmail.com>
2019-11-24 23:27:53 +0000

    Resolved issue of current sum on screen adding new input numbers to it. Now have new number input start once operator button us clicked. Also added functionality to clear button.

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const setupEventListeners = () => {
   const calculator = document.querySelector(".calc-body");
   const nonDigits = calculator.dataset.action;
   const operator = document.querySelector(".operator");
-  const digit = document.querySelector(".digit-btn");
   const screen = document.querySelector(".display-input");
   calculator.addEventListener("click", e => {
     //if the event target is a button element
@@ -21,9 +20,10 @@ const setupEventListeners = () => {
       // if the event target has no data-action set - which means it is a button with a digit
       if (!act) {
         if (
+          //use non strict equality operator as the screen is a string and the data structure is a number
           screen.value == 0 ||
-          screen.value === calcData.curSum[calcData.curSum.length - 1] ||
-          screen.value === calcData.curNums[calcData.curNums.length - 1]
+          screen.value == calcData.curSum ||
+          screen.value == calcData.curNums[calcData.curNums.length - 1]
         ) {
           screen.value = e.target.textContent;
         } else {
@@ -44,34 +44,65 @@ const setupEventListeners = () => {
       //   }
       // };
 
+      //basic arithmatic functions
       let additionFunc = num => {
         if (
-          calcData.curNums.length > 1 &&
+          //if using multiple arithmatic operators
+          calcData.curNums.length > 0 &&
+          calcData.curOp.length > 1 &&
           calcData.curOp[calcData.curOp.length - 2] === "addition"
+        ) {
+          calcData.curSum += num;
+        } else if (
+          //if only using a single arithmatic orperator e.g. 1 + 1
+          calcData.curNums.length > 0 &&
+          calcData.curOp.length < 2 &&
+          calcData.curOp[calcData.curOp.length - 1] === "addition"
         ) {
           calcData.curSum += num;
         }
       };
       let subtractFunc = num => {
         if (
-          calcData.curNums.length > 1 &&
+          calcData.curNums.length > 0 &&
+          calcData.curOp.length > 1 &&
           calcData.curOp[calcData.curOp.length - 2] === "subtract"
+        ) {
+          calcData.curSum -= num;
+        } else if (
+          calcData.curNums.length > 0 &&
+          calcData.curOp.length < 2 &&
+          calcData.curOp[calcData.curOp.length - 1] === "subtract"
         ) {
           calcData.curSum -= num;
         }
       };
       let multiplyFunc = num => {
         if (
-          calcData.curNums.length > 1 &&
+          calcData.curNums.length > 0 &&
+          calcData.curOp.length > 1 &&
           calcData.curOp[calcData.curOp.length - 2] === "multiply"
+        ) {
+          calcData.curSum *= num;
+        } else if (
+          calcData.curNums.length > 0 &&
+          calcData.curOp.length < 2 &&
+          calcData.curOp[calcData.curOp.length - 1] === "multiply"
         ) {
           calcData.curSum *= num;
         }
       };
       let divideFunc = num => {
         if (
-          calcData.curNums.length > 1 &&
+          calcData.curNums.length > 0 &&
+          calcData.curOp.length > 1 &&
           calcData.curOp[calcData.curOp.length - 2] === "divide"
+        ) {
+          calcData.curSum /= num;
+        } else if (
+          calcData.curNums.length > 0 &&
+          calcData.curOp.length < 2 &&
+          calcData.curOp[calcData.curOp.length - 1] === "divide"
         ) {
           calcData.curSum /= num;
         }
@@ -119,9 +150,9 @@ const setupEventListeners = () => {
       ) {
         calcData.curNums.push(parseFloat(screen.value)); //push input to data structure
         checkOp(act); //push operator to data structure as a string
-        screen.value = ""; //reset input field/screen to empty
-
         calcAll();
+        screen.value = calcData.curSum; //reset input field/screen to empty
+
         //console.log(calcData.curOp);
         console.log(calcData.curSum);
       }
@@ -134,6 +165,13 @@ const setupEventListeners = () => {
         multiplyFunc(parseFloat(screen.value));
         divideFunc(parseFloat(screen.value));
         screen.value = calcData.curSum;
+      }
+
+      if (act === "clear") {
+        screen.value = 0;
+        calcData.curSum = 0;
+        calcData.curNums = [];
+        calcData.curOp = [];
       }
     }
   });
